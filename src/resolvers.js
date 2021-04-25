2f41c623b293655b7e8116e2f4dad06fa04ae721
rziv <ran.ziv@gmail.com>
2021-04-25 09:29:06 +0300

    taking operationName dynamically

--- src/resolvers.js
+++ src/resolvers.js
@@ -3,7 +3,7 @@ const citizens = require("../data/preload");
 //the  IDs shouls be read dynamically (from file system or env vars) in real scenario
 const mocksIDs = [1, 3]
 
-const payload = { "operationName": "citizen", "variables": {} }
+const payload = {"variables": {} }
 
 // Provide resolver functions for your schema fields
 const resolvers = {
@@ -11,13 +11,16 @@ const resolvers = {
     citizen: async (_, { id }, context) => {
 
       payload.query = context.req.body.query
+      operationName = context.req.body.operationName
+      payload.operationName = operationName
       if (context.mocks === false && mocksIDs.includes(id)) {
         res = await axios
           .post('http://localhost:4005/graphql', payload)
         // If you wish the resolvers chain to continue,  dont send the response
         // directly thru express middleware. instead, use the following code:
-        // return res.data.data.citizen;                   
-        return context.res.send({ data: { citizen: res.data.data.citizen } });
+        // return res.data.data.citizen;
+                           
+        return context.res.send({ data: { [operationName]: res.data.data[operationName] } });
       }
       else {
         return citizens.find(c => c.id == id);
